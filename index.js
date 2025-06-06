650f9a02eeab502573a1240d5b8406d06765a391
MohamedJainulHaneef <jainulhaneef03@gmail.com>
2025-06-06 11:58:56 +0530

    Port Change

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use('/api/academic', academicRoutes);
 app.use('/api/leave', leaveRoutes);
 app.use('/api/substitution', substitutionRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
